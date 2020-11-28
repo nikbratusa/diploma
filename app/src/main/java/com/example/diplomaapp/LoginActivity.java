bd624105fe42919245e5b66e570a8a4884bd9458
nikbratusa <nik.bratusa@src.si>
2020-11-28 21:29:25 +0100

    Nadaljevanje dela na izgledu aplikacije.

--- app/src/main/java/com/example/diplomaapp/LoginActivity.java
+++ app/src/main/java/com/example/diplomaapp/LoginActivity.java
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
-    public void openUser(View view) {
-        Intent intent = new Intent(MainActivity.this, UserActivity.class);
+    public void openMain(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/diplomaapp/UserActivity.java
+++ app/src/main/java/com/example/diplomaapp/UserActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user);
     }
 
-    public void openUser(View view) {
-        Intent intent = new Intent(MainActivity.this, UserActivity.class);
+    public void openDecibels(View view) {
+        Intent intent = new Intent(UserActivity.this, DecibelsActivity.class);
         startActivity(intent);
     }
+
 }
